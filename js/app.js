8f6e4393c397bd56baca80c40e3fb5ee09e336dc
Andrei Fridrikhsen <152751636+AndreiFridrikhsen1@users.noreply.github.com>
2024-06-17 15:53:58 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,65 +2,136 @@
 const synth = window.speechSynthesis;
 const speakButton = document.getElementById('buttonSpeak');
 let textToSpeak = '';
-
+let textToSpeakArr = [];
 /* Arrays of words */
 const nouns1 = ['the turkey', 'the cat', 'the elephant', 'the dog', 'my teacher'];
-const verbs = ['sat on', 'kissed', 'danced with', 'saw', 'dosent like'];
+const verbs = ['sat on', 'kissed', 'danced with', 'saw', 'doesnt like'];
 const adjectives = ['a funny', 'a scary', 'a goofy', 'a slimy', 'a barking'];
 const nouns2 = ['goat', 'monkey', 'fish', 'cow', 'frog'];
 const places = ['on the moon', 'on the chair', 'in my spaghetti', 'in my soup', 'on the grass'];
-
+let randomNoun, randomNoun2, randomAdj, randomPlace, randomVerb;
+let duplicates = [];// keeps track of the same parts of speech so they can only be selected once
 /* Functions */
 
 // Function to generate a random word from an array
 function getRandomWord(wordArray) {
+    duplicates.push(wordArray);
     return wordArray[Math.floor(Math.random() * wordArray.length)];
+    
 }
-
-// Function to generate a random story
-function generateStory() {
-    textToSpeak = getRandomWord(nouns1) + '     ' +
+const td = document.querySelectorAll("td");
+// Function to highlight random words
+function highlighWords() {
+    // HIGHLIGHT WORDS IN THE TABLE 
+    td.forEach(i => {
+        textToSpeakArr.forEach(j => {
+            if(i.textContent.trim() === j.trim()){
+                i.style.backgroundColor = "red";
+                i.style.padding = "30px";
+                i.style.transition = "all 1.5s ease-in-out";
+            }
+        });
+    });
+    /*textToSpeak = getRandomWord(nouns1) + '     ' +
                   getRandomWord(verbs) + '      ' +
                   getRandomWord(adjectives) + '      ' +
                   getRandomWord(nouns2) + '      ' +
                   'in the     ' +
                   getRandomWord(places) + '     .';
+    */
 }
 
 // Function to speak the generated text
 function speakNow() {
+    
     const utterThis = new SpeechSynthesisUtterance(textToSpeak);
     synth.speak(utterThis);
+    
 }
 
 /* Event Listeners */
+buttons = document.querySelectorAll('button')
+buttons.forEach(button => {
+    button.addEventListener('click', function(e){
+        e.target.style.backgroundColor = "red";
+        if(e.target.textContent.trim()=="Speak the generated story"){
+            buttons.forEach(i=> {
+                i.style.backgroundColor = "";
+            })
+        }
+    });
+});
 
 // Event listener for each button to generate random words
 document.getElementById('buttonNoun').addEventListener('click', function () {
-    textToSpeak += getRandomWord(nouns1) + ' ';
+    if(!duplicates.includes(nouns1)){
+        randomNoun = getRandomWord(nouns1);
+        textToSpeak += randomNoun + ' ';
+        textToSpeakArr.push(randomNoun);
+    }
+  
+   
+
 });
 
 document.getElementById('buttonVerb').addEventListener('click', function () {
-    textToSpeak += getRandomWord(verbs) + ' ';
+    
+    if(!duplicates.includes(verbs)){
+        randomVerb = getRandomWord(verbs);
+        textToSpeak += randomVerb + ' ';
+        textToSpeakArr.push(randomVerb);
+    }
 });
 
 document.getElementById('buttonAdjective').addEventListener('click', function () {
-    textToSpeak += getRandomWord(adjectives) + ' ';
+     
+     if(!duplicates.includes(adjectives)){
+        randomAdj = getRandomWord(adjectives);
+        textToSpeak += randomAdj + ' ';
+        textToSpeakArr.push(randomAdj);
+     }
 });
 
 document.getElementById('buttonNoun2').addEventListener('click', function () {
-    textToSpeak += getRandomWord(nouns2) + ' ';
+    
+    if(!duplicates.includes(nouns2)){
+        randomNoun2 = getRandomWord(nouns2);
+        textToSpeak += randomNoun2 + ' ';
+        textToSpeakArr.push(randomNoun2);
+    }
 });
 
 document.getElementById('buttonPlace').addEventListener('click', function () {
-    textToSpeak += getRandomWord(places) + '.';
+    
+    if(!duplicates.includes(places)){
+        randomPlace = getRandomWord(places);
+        textToSpeak += randomPlace + ' ';
+        textToSpeakArr.push(randomPlace);
+    }
 });
 
 // Event listener for the speak button
-speakButton.addEventListener('click', speakNow);
+//speakButton.addEventListener('click', speakNow);
 
+function resetCells(){
+    td.forEach(cell => {
+        cell.style.backgroundColor = "";
+        cell.style.padding = "";
+    })
+}
 // Event listener for generating a random story
 document.getElementById('buttonRandomStory').addEventListener('click', function () {
-    generateStory();
-    speakNow();
+    if(textToSpeakArr.length<5){
+        alert("Please, select all the parts of speech");
+    }else{
+        highlighWords();
+        speakNow();
+    }
+    //reset for a new game
+    duplicates = []
+    textToSpeak = "";
+    textToSpeakArr = [];
+    const timeOut = setTimeout(resetCells, 2000);
+    
+    
 });
